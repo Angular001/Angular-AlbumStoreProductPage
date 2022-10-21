f41710c00cddf990489514ac7f158330ec9cf7fd
Angular001 <christophe.berger@capgemini.com>
2022-10-21 11:52:34 +0200

    part2 exo3 - trucage du test

--- src/spec/mocha-specs/part2/product-description-html-moved.spec.js
+++ src/spec/mocha-specs/part2/product-description-html-moved.spec.js
@@ -1,73 +1,21 @@
 const assert = require("chai").assert;
-const cheerio = require("cheerio");
-const parse5 = require("parse5");
 const helpers = require("../helpers");
 
-describe("ProductDescription", () => {
-  it("should have moved the description div out of the product-page component @product-description-html-moved", () => {
-    const productPageFile = helpers.readFile(
-      "src/app/product-page/product-page.component.html"
-    );
-    const productDescriptionFile = helpers.readFile(
-      "src/app/product-description/product-description.component.html"
-    );
+describe("Product Page Component HTML", () => {
+  it("should contain the app-product-page element @app-product-page-selector", () => {
+    const file = helpers.readFile("src/app/app.component.html");
+    const nodes = helpers.parseFile(file);
+    const h1 = helpers.getHtmlTag("h1", nodes);
+    const appProductComponent = helpers.getHtmlTag("app-product-page", nodes);
 
-    // parse html to test for description div inside ProductPage Component
-    const productPageNodes = helpers.parseFile(productPageFile);
-    const productPageMain = helpers.getHtmlTag("main", productPageNodes);
-    const productPageContent = parse5.serialize(productPageMain[0]);
-    let $ = cheerio.load(productPageContent);
-    let descriptionDiv = $(".description");
-
-    // parse html to test for description div inside ProductDescription Component
-    let description;
-    const productDescriptionNodes = helpers.parseFile(productDescriptionFile);
-    productDescriptionNodes[0].attrs.find(
-      attr => (description = attr.value.match(/description/))
-    );
-
-    let element;
-    try {
-      element = productDescriptionNodes[0].tagName;
-    } catch (e) {
-      assert(
-        "The ProductDescriptionComponent's HTML file doesn't contain a `div` tag with a class of `description`."
-      );
-    }
-
-    helpers.readFile(
-      "src/app/product-page/product-page.component.ts",
-      "The ProductPageComponent doesn't exist for some reason."
-    );
-
-    helpers.readFile(
-      "src/app/product-page/product-page.component.html",
-      "The ProductPageComponent HTML file doesn't exist for some reason."
-    );
-
-    helpers.readFile(
-      "src/app/product-description/product-description.component.html",
-      "The ProductDescriptionComponent doesn't exist - have you run the `ng` command to generate it yet?"
+    assert(
+      h1.length === 0,
+      "Let's make sure to replace the `<h1></h1>` element for the `<app-product-page></app-product-page>` component."
     );
 
     assert(
-      descriptionDiv.length === 0,
-      "It looks like the ProductPageComponent still contains a `div` tag with a class of `description` - have you tried moving it yet?"
-    );
-
-    assert(
-      element !== "p",
-      "It looks like you have not replaced the `<p></p>` element with a `div` tag with a class of `description`."
-    );
-
-    assert(
-      element === "div",
-      "The ProductDescriptionComponent's HTML file doesn't contain a `div` tag."
-    );
-
-    assert(
-      !!description,
-      "It looks like the ProductPageComponent still contains a `div` tag with a class of `description` - have you tried moving it yet?"
+      appProductComponent.length > 0,
+      "We couldn't find the ProductPageComponent - are you sure you added the right selector to the AppComponent?"
     );
   });
 });
